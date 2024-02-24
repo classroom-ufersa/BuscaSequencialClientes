12bca21eb3f5a12671fc48f3761d351814bf49bb
hillaryds <hillary.saldanha199@gmail.com>
2024-02-24 19:48:45 -0300

    refactor: added numero_inteiroc, verifica_codigo, cabecalho and menu functions

--- linguagem_c/cliente.h
+++ linguagem_c/cliente.h
@@ -4,8 +4,17 @@
 typedef struct cliente Cliente;
 
 /* Função responsável pela busca sequencial de clientes a partir do seu código*/
-void procura_codigo(none);
+void procura_codigo();
 
 
 /* Função responsável pela busca sequencial de clientes a partir do seu nome*/
-void procura_nome(none);
+void procura_nome();
+
+
+int numero_inteiroc(char *str);
+
+int verifica_codigo(char *codigo_digitado);
+
+void cabecalho(char* linha, char* titulo);
+
+void menu();
