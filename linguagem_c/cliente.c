2e4611ba79354a035baefef9e983206fdfc1b044
alexrbss <alexandre.rbss@gmail.com>
2024-02-24 21:25:27 -0300

    fix: procura_codigo function fixed

--- linguagem_c/cliente.c
+++ linguagem_c/cliente.c
@@ -20,37 +20,39 @@ int numero_inteiroc(char *str) {
     return 1;
 }
 
-int verifica_codigo(char *codigo_digitado){
+int verifica_codigo(char *codigo_digitado) {
     char linha[300];
     char codigo_arquivo[100];
     char cidade_arquivo[100];
     char nome_arquivo[100];
-    FILE *arquivo = fopen("clientes.txt", "r"); // Abre em modo de leitura
+    FILE *arquivo = fopen("clientes.txt", "r"); //abre em modo de leitura
     
     if (arquivo == NULL) {
-        return 0; // verifica se o arquivo foi aberto ou não
+        return 0; //verifica se o arquivo foi aberto ou não
     }
 
-    while (fgets(linha, sizeof(linha), arquivo) != NULL) { //lê o arquivo linha por linha 
+    //lê o arquivo linha por linha 
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) { 
         if (sscanf(linha, "%99[^\t]\t%99[^\t]\t%99[^\t ]", nome_arquivo, cidade_arquivo, codigo_arquivo) == 3) {
-            // Verifica se o código lido é igual ao código fornecido
+            //verifica se o código lido é igual ao código fornecido
             if (strcmp(codigo_arquivo, codigo_digitado) == 0) {
                 printf("\nCodigo ja existe.");
                 fclose(arquivo);
-                return 1; // Código encontrado
+                return 1; //código encontrado
             }
         }
     }
 
     fclose(arquivo);
-    return 0; // Código não encontrado
+    return 0; //código não encontrado
 }
 
 void adiciona_cliente() {
     FILE *arquivo = fopen("clientes.txt", "a"); //abre o arquivo no modo de escrita
     Cliente cliente;
     
-    if(arquivo == NULL) { //verifica se o arquivo foi aberto ou não
+    //verifica se o arquivo foi aberto
+    if(arquivo == NULL) { 
         printf("Erro ao abrir o aquivo.\n");
         return;
     }
@@ -60,7 +62,7 @@ void adiciona_cliente() {
     cabecalho("============", "Adiconar Cliente");
     printf("\nDigite o nome do cliente: ");
     scanf(" %99[^\n]", cliente.nome);
-    getchar(); // Limpa o buffer de entrada
+    getchar(); //limpa o buffer de entrada
     printf("\nDigite a cidade do cliente: ");
     scanf(" %99[^\n]", cliente.cidade);
     getchar();
@@ -76,18 +78,19 @@ void adiciona_cliente() {
 
     strcpy(cliente.codigo, codigo_digitado);
 
-    // Escrever os dados do cliente no arquivo 'clientes.txt'
+    //escreve os dados do cliente no arquivo 'clientes.txt'
     fprintf(arquivo, "\n%s \t %s \t %s \n", cliente.nome, cliente.cidade, cliente.codigo);
 
     fclose(arquivo);
 }
 
 void procura_codigo() {
-    FILE *arquivo = fopen("clientes.txt", "r");
+    FILE *arquivo = fopen("clientes.txt", "r"); //abre o arquivo no modo de escrita
     
     char codigo[100], linha[256];
     int encontrou = 0;
 
+    //verifica se o arquivo foi aberto
     if(arquivo == NULL) { 
         printf("Erro ao abrir o aquivo.\n");
         return;
@@ -97,12 +100,23 @@ void procura_codigo() {
     printf("Digite o código do cliente: ");
     fgets(codigo, 100, stdin);
     codigo[strcspn(codigo, "\n")] = 0; //função que garante que a string termine no local onde o usuário pressionou enter
-    printf("%s \t %s \t %s\n", "Nome", "Cidade", "Codigo");
 
+    //verifica se o código contém apenas números
+    for(int index = 0; codigo[index] != '\0'; index++) {
+        if(!isdigit(codigo[index])) {
+            printf("O codigo deve conter apenas numeros.\n");
+            fclose(arquivo);
+            return;
+        }
+    }
+
+    printf("%-30s%-30s%-10s", "NOME", "CIDADE", "CODIGO");
+
+    //lê o arquivo linha por linha 
     while(fgets(linha, sizeof(linha), arquivo)) {
         char *token = strtok(linha, "\t"); //divide a string em partes com base no delimitador \t
         
-        //realiza a comparação entre o código fornecido pelo usuário e os existentes
+        //verifica se o token atual é igual ao codigo desejado
         if(strcmp(token, codigo) == 0) {
             printf("%s", linha);
             encontrou = 1;
@@ -118,11 +132,12 @@ void procura_codigo() {
 }
 
 void procura_nome() {
-    FILE *arquivo = fopen("clientes.txt", "r");
+    FILE *arquivo = fopen("clientes.txt", "r"); //abre o arquivo no modo de escrita
     
     char nome[100], linha[256];
     int encontrou = 0;
 
+    //verifica se o arquivo foi aberto
     if(arquivo == NULL) {
         printf("Erro ao abrir o aquivo.\n");
         return;
@@ -133,11 +148,13 @@ void procura_nome() {
     fgets(nome, 100, stdin);
     nome[strcspn(nome, "\n")] = 0; //função que garante que a string termine no local onde o usuário pressionou enter
 
-    printf("%s \t %s \t %s\n", "Nome", "Cidade", "Codigo");
+    printf("%-30s%-30s%-10s", "NOME", "CIDADE", "CODIGO");
     
+    //lê o arquivo linha por linha 
     while(fgets(linha, sizeof(linha), arquivo)) {
         char *token = strtok(linha, "\t"); //divide a string em partes com base no delimitador \t
 
+        //verifica se o token atual é igual ao nome desejado
         if(strcmp(token, nome) == 0) {
             printf("%s", linha);
             encontrou = 1;
@@ -152,9 +169,10 @@ void procura_nome() {
 }
 
 void lista_clientes(){
-    FILE *arquivo = fopen("clientes.txt", "r");
+    FILE *arquivo = fopen("clientes.txt", "r"); //abre o arquivo no modo de escrita
     char linha[256];
 
+    //verifica se o arquivo foi aberto
     if(arquivo == NULL) {
         printf("Erro ao abrir o aquivo.\n");
         return;
@@ -163,6 +181,7 @@ void lista_clientes(){
     cabecalho("=", "Lista de clientes");
     printf("%s \t %s \t %s\n", "Nome", "Cidade", "Codigo");
 
+    //lê o arquivo linha por linha 
     while(fgets(linha, sizeof(linha), arquivo)) {
         printf("%s", linha);
     }
