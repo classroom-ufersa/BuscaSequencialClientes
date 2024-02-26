c6ded0d68d633b67f7799aeee73603d98156a3ee
biiaalmeida <abeatrizpb@gmail.com>
2024-02-26 12:40:35 -0300

    Docs: Removed comments

--- linguagem_python/cliente.py
+++ linguagem_python/cliente.py
@@ -23,19 +23,15 @@ def adiciona_cliente():
         None
     """
 
-    # abrir o arquivo 'clientes.txt' em modo de escrita
     arquivo = open('clientes.txt', 'a')
     
-    # criação de um dicionário vazio chamado clientes
     cliente = {}
     
     cabeçalho("-", "Adicionar cliente")
     
-    # solicitam os dados do usuário
     nome = (input("Digite o nome do cliente: ")).title()
     cidade = input("Digite a cidade do cliente: ")
 
-    # verificando se o código é um inteiro
     while True:
         try:
             codigo = int(input("Digite o código de cliente: "))
@@ -46,9 +42,7 @@ def adiciona_cliente():
     while True:
         if verifica_codigo(codigo):
             print("Código já existente!")
-            # verificando se o código é um inteiro
     
-            # verificando novamente se é um inteiro
             while True:
                 try:
                     codigo = int(input("Digite o código de cliente: "))
@@ -63,7 +57,6 @@ def adiciona_cliente():
     cliente['cidade'] = cidade
     cliente['codigo'] = codigo
     
-    # Os dados do cliente são escritos no arquivo 'clientes.txt'
     arquivo.write(f"{cliente['nome']}\t{cliente['cidade']}\t{cliente['codigo']}\n")
     arquivo.close()
 
@@ -75,14 +68,12 @@ def lista_clientes():
         None
     """
 
-    # abre o arquivo 'clientes.txt' no modo leitura
     arquivo = open('clientes.txt', 'r')
     
     print("-" * 50)
     print(f"{'Nome':<20}{'Cidade':<20}{'Código':<20}")
     print("-" * 50)
     
-    # laço de repetição para listar os clientes
     for linha in arquivo:
         resultado = linha.split("\t")
         print(f"{resultado[0]:<20}{resultado[1]:<20}{resultado[2]:<20}")
@@ -98,10 +89,8 @@ def procura_codigo():
         None
     """
 
-    # abre o arquivo 'clientes.txt' em modo leitura 
     arquivo = open('clientes.txt', 'r')
     
-    # verificando se o código é um inteiro
     while True:
         try:
             codigo = int(input("Digite o código de cliente: "))
@@ -113,13 +102,11 @@ def procura_codigo():
     
     cabeçalho("-", "Resultado da busca por código")
     
-    # laço de repetição referente a busca sequencial pelo codigo (linha por linha)
     for linha in arquivo:
         
         resultado = linha.split("\t")
         resultado[2] = resultado[2].replace("\n", "")
         
-        # caso a busca seja bem-sucedida, imprime os dados dos clientes
         if str(codigo) in resultado:
             busca = True
             print("-" * 50)
@@ -129,13 +116,11 @@ def procura_codigo():
             break;
     print("-" * 50)
     
-    # caso a busca não for bem sucedida, será exibido que o cliente não foi encontrado
     if busca == False:
         print("Cliente não encontrado!")
         
     a = input("Pressione enter para continuar...")
 
-    # fecha o arquivo 'clientes.txt' que foi aberto anteriomente para leitura
     arquivo.close()
     
 def procura_nome():
@@ -151,11 +136,9 @@ def procura_nome():
     
     cabeçalho("-", "Resultado da busca por nome")
     
-    # laço de repetição referente a busca sequencial pelo nome do cliente (linha por linha)
     for linha in arquivo:
         resultado = linha.split("\t")
 
-        # caso a busca seja bem-sucedida, imprime os dados dos clientes
         if nome in resultado[0]:
             busca = True
             print("-" * 50)
@@ -164,12 +147,10 @@ def procura_nome():
             print(f"{resultado[0]:<20}{resultado[1]:<20}{resultado[2]:<20}")
     print("-" * 50)
     
-    # caso a busca não for bem sucedida, será exibido que o cliente não foi encontrado
     if busca == False:
         print("Cliente não encontrado!")
         
     a = input("Pressione enter para continuar...")
-    # fecha o arquivo 'clientes.txt' que foi aberto anteriomente para leitura
     arquivo.close()
 
 def verifica_codigo(codigo):
@@ -184,13 +165,11 @@ def verifica_codigo(codigo):
 
     busca = False
     
-    # laço de repetição referente a busca sequencial da existência do código no arquivo(linha por linha)
     for linha in arquivo:
         
         resultado = linha.split("\t")
         resultado[2] = resultado[2].replace("\n", "")
 
-        # caso o código exista no arquivo
         if str(codigo) in resultado:
             busca = True
             break;
